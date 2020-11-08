019b2dff3804435a2ce12e548e5b0eedf2c20ec3
kyleyalch <73710716+kyleyalch@users.noreply.github.com>
2020-11-08 17:44:35 -0500

    Delete program logos.js

--- program logos.js
+++
@@ -1,7 +0,0 @@
-var imgps = document.getElementById("ImgPs"); // Photoshop Logo
-var imgae = document.getElementById("ImgAe"); // AfterFX Logo
-var imgc4d = document.getElementById("ImgC4D"); // C4D Logo
-
-impgs.onhover = function() {
-	imgps.style.transform.scale = "1.2";
-}
