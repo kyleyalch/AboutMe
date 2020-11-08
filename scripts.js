d3553fbd233757300293750b085c717fc94758a4
kyleyalch <73710716+kyleyalch@users.noreply.github.com>
2020-11-08 17:44:08 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Scripts for the "Design" tab and the program icons
+function psText() {
+  document.getElementById("dzn").style.color = "#009ADA";
+  document.getElementById("dzn").innerHTML = "Adobe Photoshop";
+}
+function aeText() {
+  document.getElementById("dzn").style.color = "purple";
+  document.getElementById("dzn").innerHTML = "Adobe After Effects";
+}
+function c4dText() {
+  document.getElementById("dzn").style.color = "#1057FF";
+  document.getElementById("dzn").innerHTML = "MAXON Cinema 4D";
+}
+function Alert() {
+  alert("Hey! I told you not to touch that!");
+}
